bb6e8603143cd3ce0965cc76de83b99200a03eb9
Benjamim <benjamimmpr@gmail.com>
2026-04-25 11:16:58 +0000

    Aula 6

---
+++ tesouro/tesouro.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int A, N, res;
+
+    cin >> A;
+    cin >> N;
+
+    res = (2 * A) / (N + 2);
+
+    cout << res;
+
+    return 0;
+}
